c43c69a41b3c274e96193c63bdd98909093088f7
DucAnh <daoducanhc@gmail.com>
2021-08-17 22:21:18 +0700

    add detail BaseUser.cpp

--- BaseUser.cpp
+++ BaseUser.cpp
@@ -1,99 +1,115 @@
 #include "BaseUser.h"
 
-FreshUser::FreshUser(uint id, string name, uint age, uint heightCm, string hobbyList, vector<uint> friendList, string className)
+
+FreshUser::FreshUser(uint id, string name, uint age, uint heightCm, string hobbyList, vector<uint> friendList, string className) 
+	: BaseUser(id, UserType::Fresh, name, age, heightCm, hobbyList, friendList)
 {
+	this->className = className;
 }
 
 string FreshUser::getClassName()
 {
-	return string();
+	return this->className;
 }
 
 void FreshUser::setClassName(string a)
 {
-}
-
-
-BaseUser::BaseUser(uint id, string name, uint age, uint heightCm, string hobbyList, vector<uint> friendList)
-{
+	this->className = a;
 }
 
 BaseUser::BaseUser(uint id, UserType type, string name, uint age, uint heightCm, string hobbyList, vector<uint> friendList)
 {
+	this->id = id;
+	this->type = type;
+	this->name = name;
+	this->age = age;
+	this->heightCm = heightCm;
+	this->hobbyList = hobbyList;
+	this->friendList = friendList;
 }
 
 uint BaseUser::getId()
 {
-	return uint();
+	return this->id;
 }
 
 UserType BaseUser::getType()
 {
-	return UserType();
+	return this->type;
 }
 
 string BaseUser::getName()
 {
-	return string();
+	return this->name;
 }
 
 uint BaseUser::getAge()
 {
-	return uint();
+	return this->age;
 }
 
 uint BaseUser::getHeightCm()
 {
-	return uint();
+	return this->heightCm;
 }
 
 string BaseUser::getHobbyList()
 {
-	return string();
+	return this->hobbyList;
 }
 
 vector<uint> BaseUser::getFriendList()
 {
-	return vector<uint>();
+	return this->friendList;
 }
 
 void BaseUser::setId(uint a)
 {
+	this->id = a;
 }
 
 void BaseUser::setType(UserType a)
 {
+	this->type = a;
 }
 
 void BaseUser::setName(string a)
 {
+	this->name = a;
 }
 
 void BaseUser::setAge(uint a)
 {
+	this->age = a;
 }
 
 void BaseUser::setHeightCm(uint a)
 {
+	this->heightCm = a;
 }
 
 void BaseUser::setHobbyList(string a)
 {
+	this->hobbyList = a;
 }
 
 void BaseUser::setFriendList(vector<uint> a)
 {
+	this->friendList = a;
 }
 
-PermanentUser::PermanentUser(uint id, string name, uint age, uint heightCm, string hobbyList, vector<uint> friendList, string departmentName)
+PermanentUser::PermanentUser(uint id, string name, uint age, uint heightCm, string hobbyList, vector<uint> friendList, string departmentName) 
+	: BaseUser(id, UserType::Permanent, name, age, heightCm, hobbyList, friendList)
 {
+	this->departmentName = departmentName;
 }
 
 string PermanentUser::getDepartmentName()
 {
-	return string();
+	return this->departmentName;
 }
 
 void PermanentUser::setDepartmentName(string a)
 {
+	this->departmentName = a;
 }
